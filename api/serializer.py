ee3162851a3d2c5f22b6e44e8753221864c133a2
linja <kiensiau1130@gmail.com>
2021-11-08 21:45:34 +0800

    Update Database

--- api/serializer.py
+++ api/serializer.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username','password','userEmail']
+        fields = ['userName','password','userEmail']
         extra_kwargs = {
             'password':{'write_only':True},
         }
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
     def save(self):
         user = User(
             userEmail=self.validated_data['userEmail'],
-            username=self.validated_data['username']
+            username=self.validated_data['userName']
         )
         password = self.validated_data['password']
         user.set_password(password)
@@ -22,13 +22,13 @@ class UserSerializer(serializers.ModelSerializer):
 class UpdateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username','userEmail','userImage','userPhone']
+        fields = ['userName','userEmail','userImage','userPhone']
         # extra_kwargs = {
         #     'password':{'write_only':True},
         # }
 
     def update(self,instance,validated_data):
-        instance.username = validated_data['username']
+        instance.username = validated_data['userName']
         instance.userEmail = validated_data['userEmail']
         instance.userPhone = validated_data['userPhone']
         instance.userImage = validated_data['userImage']
