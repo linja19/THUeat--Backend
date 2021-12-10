f751e8cdacd792b637e3185a2b4cef828d78ac21
linja <kiensiau1130@gmail.com>
2021-12-11 00:32:43 +0800

    fix: change error message

--- api/error_handler.py
+++ api/error_handler.py
@@ -5,7 +5,15 @@ def custom_exception_handler(exc, context):
     # ovverride IsAuthenticated permission class exception
     if (response.data['detail'].code == 'not_authenticated'):
         response.data['code'] = 400
-        response.data['message'] = "Token not provided"
+        response.data['message'] = "口令有误"
+        del response.data['detail']
+    elif (response.data['detail'].code == 'authentication_failed'):
+        response.data['code'] = 400
+        response.data['message'] = "口令有误"
+        del response.data['detail']
+    elif (response.data['detail'].code == 'permission_denied'):
+        response.data['code'] = 400
+        response.data['message'] = "没有权限"
         del response.data['detail']
 
     return response
